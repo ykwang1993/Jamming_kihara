58821ddd4ad9a321b05660b2d423fa42092fd288
ykwang1993 <ykwang1993@gmail.com>
2016-10-15 23:17:01 +0800

    Add files via upload

--- kihara.c
+++ kihara.c
@@ -6,6 +6,7 @@
 #define dot(v1,v2) ((v1)[0]*(v2)[0] + (v1)[1]*(v2)[1])
 #define cross(v1,v2) ((v1)[0]*(v2)[1]-(v1)[1]*(v2)[0])
 #define length(v) sqrt((v)[0]*(v)[0]+(v)[1]*(v)[1])
+#define swap(x, y) do{int tmp = x; x = y; y = tmp;} while(0)
 
 typedef double real;
 
@@ -48,8 +49,8 @@ void point_rod_dist(real *p, rod *r, real *info){
 		info[0]=fabs(cross(v,v1))/length(v);
 		
 		//find the collision point
-		real a=v[0];
-		real b=-v[1];
+		real a=v[1];
+		real b=-v[0];
 		real c=-a*(r->x_1[0])-b*(r->x_1[1]); //c=-ax-by
 		
 		info[1]=p[0]-a*(a*p[0]+b*p[1]+c)/(a*a+b*b);
@@ -69,13 +70,31 @@ void rod_dist(rod *r1, rod *r2){
 	point_rod_dist(r2->x_1,r1,&dist[2][0]);
 	point_rod_dist(r2->x_2,r1,&dist[3][0]);
 	
-	/* for(int i=0;i<4;i++)
+	//for sorting the min dist 
+	int idx[]={0,1,2,3};
+	real min_dist[4];
+	for(int i=0;i<4;i++)
+		min_dist[i]=dist[i][0];
+	
+	
+	for(int i = 0; i < 4; i++)
+        for(int j = 1; j < 4 - i; j++)
+            if(min_dist[j] < min_dist[j-1]){
+				swap(idx[j], idx[j-1]);
+				swap(min_dist[j], min_dist[j-1]);
+			}
+				
+
+            
+
+        
+
+    
+	
+	for(int i=0;i<4;i++)
 		printf("%f %f %f\n",dist[i][0],dist[i][1],dist[i][2]);
-	
-	real info[3];
-	point_rod_dist(r1->x_1,r2,info);
-	printf("\n%f %f %f\n",info[0],info[1],info[2]); */
-	
+	for(int i=0;i<4;i++)
+		printf("%d ",idx[i]);
 }
 
 int main(){
@@ -94,13 +113,14 @@ int main(){
 	
 	rod_dist(&r1,&r2);
 	
-	real x1[] = {1.,1.0};
 	
-	real info[3];
-	point_rod_dist(x1,&r1,info);
+	//real x1[] = {2.,2.};
 	
-	printf("min_dist:%f\n", info[0]);
-	printf("collision point:(%f,%f)\n",info[1],info[2]);
+	//real info[3];
+	//point_rod_dist(x1,&r1,info);
+	
+	//printf("min_dist:%f\n", info[0]);
+	//printf("collision point:(%f,%f)\n",info[1],info[2]);
 	
 	return 0;
 }
